3f037926bf407a674350ebe715881c07235a1d53
sachinakdatta <sachinakdatta@gmail.com>
2026-01-01 22:41:31 +0530

    Delete beckend directory

--- beckend/services/drive.py
+++
@@ -1,6 +0,0 @@
-def export_project(data):
-    project = data.get("project")
-    return {
-        "message": f"Project {project} exported to Google Drive (mock)",
-        "status": "success"
-    }
